6555f35e6a9b03c7635d3dd0f19481dc0985d52f
Flyover1598 <tai@upward.jp>
2025-06-19 16:39:40 +0900

    Written IntegrationTest for getAuthorsList

--- src/test/java/com/example/javaOjt/integrationTests/AuthorIntegrationTest.java
+++ src/test/java/com/example/javaOjt/integrationTests/AuthorIntegrationTest.java
@@ -1,9 +1,12 @@
 package com.example.javaOjt.integrationTests;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.example.javaOjt.DBTestBase;
 import com.example.javaOjt.beans.responses.author.GetAuthorResponse;
+import com.example.javaOjt.beans.responses.author.GetAuthorsResponse;
+import com.example.javaOjt.beans.responses.author.GetAuthorsResponse.AuthorInfo;
 import com.example.javaOjt.beans.responses.common.OjtExceptionResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +20,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 class AuthorIntegrationTest extends DBTestBase {
 
-  private static final String AUTHOR_BASE_URL = "/authors/";
+  private static final String AUTHOR_BASE_URL = "/authors";
 
   @Autowired
   private MockMvc mockMvc;
@@ -31,15 +34,15 @@ class AuthorIntegrationTest extends DBTestBase {
 
     // Perform the GET request to retrieve an existing author
     MvcResult result = mockMvc.perform(
-        MockMvcRequestBuilders.get(AUTHOR_BASE_URL + targetId))
-      .andExpect(MockMvcResultMatchers.status().isOk())
-      .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-      .andReturn();
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL + "/" + targetId))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
 
     // Parse the response to GetAuthorResponse object
     GetAuthorResponse resultAuthor = objectMapper.readValue(
-      result.getResponse().getContentAsString(),
-      GetAuthorResponse.class);
+        result.getResponse().getContentAsString(),
+        GetAuthorResponse.class);
 
     // Assertions to verify the author details
     Assertions.assertNotNull(resultAuthor);
@@ -53,21 +56,21 @@ class AuthorIntegrationTest extends DBTestBase {
 
     // Attempt to get an author that does not exist
     MvcResult result = mockMvc.perform(
-        MockMvcRequestBuilders.get(AUTHOR_BASE_URL + targetId))
-      .andExpect(MockMvcResultMatchers.status().isNotFound())
-      .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-      .andReturn();
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL + "/" + targetId))
+        .andExpect(MockMvcResultMatchers.status().isNotFound())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
 
     // Parse the error response
     OjtExceptionResponse resultErr = objectMapper.readValue(
-      result.getResponse().getContentAsString(),
-      OjtExceptionResponse.class);
+        result.getResponse().getContentAsString(),
+        OjtExceptionResponse.class);
 
     // Assertions to verify the error response
     Assertions.assertNotNull(resultErr);
     Assertions.assertNotNull(resultErr.getError());
     Assertions.assertEquals("Author not found with ID: " + targetId,
-      resultErr.getError().message());
+        resultErr.getError().message());
     Assertions.assertEquals(404, resultErr.getError().code());
   }
 
@@ -77,16 +80,16 @@ class AuthorIntegrationTest extends DBTestBase {
 
     // Perform the GET request to retrieve an existing author
     MvcResult result = mockMvc.perform(
-        MockMvcRequestBuilders.get(AUTHOR_BASE_URL + targetId)
-          .param("withBooks", "true"))
-      .andExpect(MockMvcResultMatchers.status().isOk())
-      .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-      .andReturn();
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL + "/" + targetId)
+                .param("withBooks", "true"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
 
     // Parse the response to GetAuthorResponse object
     GetAuthorResponse resultAuthor = objectMapper.readValue(
-      result.getResponse().getContentAsString(),
-      GetAuthorResponse.class);
+        result.getResponse().getContentAsString(),
+        GetAuthorResponse.class);
 
     // Assertions to verify the author details
     Assertions.assertNotNull(resultAuthor);
@@ -97,7 +100,117 @@ class AuthorIntegrationTest extends DBTestBase {
     Assertions.assertEquals(1, resultAuthor.getBooks().size());
     Assertions.assertEquals(1, resultAuthor.getBooks().getFirst().id());
     Assertions.assertEquals("AWSコスト最適化ガイドブック",
-      resultAuthor.getBooks().getFirst().title());
+        resultAuthor.getBooks().getFirst().title());
     Assertions.assertEquals("2023-03-29", resultAuthor.getBooks().getFirst().publishedAt());
   }
+
+  @Test
+  void getAuthorsTest_noParam() throws Exception {
+    // Perform the GET request to retrieve authors
+    MvcResult result = mockMvc.perform(
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    // Parse the response to GetAuthorsResponse object
+    GetAuthorsResponse resultAuthors = objectMapper.readValue(
+        result.getResponse().getContentAsString(), GetAuthorsResponse.class);
+
+    // Assertions to verify the details
+    Assertions.assertNotNull(resultAuthors);
+    Assertions.assertEquals(4, resultAuthors.getAuthorsSummaryList().size());
+    Assertions.assertEquals(1, resultAuthors.getAuthorsSummaryList().getFirst().id());
+    Assertions.assertEquals("門畑顕博", resultAuthors.getAuthorsSummaryList().getFirst().name());
+    Assertions.assertEquals(4, resultAuthors.getAuthorsSummaryList().getLast().id());
+    Assertions.assertEquals("J. K. Rowling",
+        resultAuthors.getAuthorsSummaryList().getLast().name());
+  }
+
+  @Test
+  void getAuthorsTest_allParam() throws Exception {
+    // Perform the GET request to retrieve authors
+    MvcResult result = mockMvc.perform(
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL).param("sort_by", "name").param("order", "dsc")
+                .param("ids", "2, 3, 10"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    // Parse the response to GetAuthorsResponse object
+    GetAuthorsResponse resultAuthors = objectMapper.readValue(
+        result.getResponse().getContentAsString(), GetAuthorsResponse.class);
+
+    // Assertions to verify the details
+    Assertions.assertNotNull(resultAuthors);
+    Assertions.assertEquals(2, resultAuthors.getAuthorsSummaryList().size());
+    Assertions.assertEquals(3, resultAuthors.getAuthorsSummaryList().getFirst().id());
+    Assertions.assertEquals("太宰治", resultAuthors.getAuthorsSummaryList().getFirst().name());
+    Assertions.assertEquals(2, resultAuthors.getAuthorsSummaryList().getLast().id());
+    Assertions.assertEquals("夏目漱石",
+        resultAuthors.getAuthorsSummaryList().getLast().name());
+  }
+
+  @Test
+  void getAuthorsTest_illegalAttribute() throws Exception {
+    // Perform the GET request to retrieve authors
+    MvcResult result = mockMvc.perform(
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL).param("sort_by", "v50"))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    // Parse the error response
+    OjtExceptionResponse resultErr = objectMapper.readValue(
+        result.getResponse().getContentAsString(),
+        OjtExceptionResponse.class);
+
+    // Assertions to verify the error response
+    Assertions.assertNotNull(resultErr);
+    Assertions.assertNotNull(resultErr.getError());
+    Assertions.assertEquals("Invalid sort_by attribute",
+        resultErr.getError().message());
+    Assertions.assertEquals(400, resultErr.getError().code());
+  }
+
+  @Test
+  void getAuthorsTest_illegalOrder() throws Exception {
+    // Perform the GET request to retrieve authors
+    MvcResult result = mockMvc.perform(
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL).param("order", "kfc"))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    // Parse the error response
+    OjtExceptionResponse resultErr = objectMapper.readValue(
+        result.getResponse().getContentAsString(),
+        OjtExceptionResponse.class);
+
+    // Assertions to verify the error response
+    Assertions.assertNotNull(resultErr);
+    Assertions.assertNotNull(resultErr.getError());
+    Assertions.assertEquals("Invalid order",
+        resultErr.getError().message());
+    Assertions.assertEquals(400, resultErr.getError().code());
+  }
+
+  @Test
+  void getAuthorsTest_emptyList() throws Exception {
+    // Perform the GET request to retrieve authors
+    MvcResult result = mockMvc.perform(
+            MockMvcRequestBuilders.get(AUTHOR_BASE_URL).param("ids", "65535"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+        .andReturn();
+
+    // Parse the error response
+    GetAuthorsResponse resultEmptyList = objectMapper.readValue(
+        result.getResponse().getContentAsString(), GetAuthorsResponse.class);
+
+    // Assertions to verify the error response
+    Assertions.assertNotNull(resultEmptyList);
+    Assertions.assertEquals(new ArrayList<AuthorInfo>(), resultEmptyList.getAuthorsSummaryList());
+  }
+
 }
